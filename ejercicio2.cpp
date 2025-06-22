933b27473dd0e63ce30da4ce0616c186af14cd98
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-06-22 18:35:36 -0500

    Ejercicio 2 – agregando saltos de linea, versión final

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main (){
+int main() {
+	
+    char caracter[60]; 
+    cout<<"ECO DE LA ENTRADA"<<endl<<endl;
+    cout << "Comencemos"<<endl;
+    cout << "1) Escribe cualquier frase que desees: "<<endl;
+    cin.getline(caracter, 60); 
+    cout <<endl<<"2) Cada palabra de la frase, escrita en una linea diferente:" << endl;
+    for(int i = 0; caracter[i] !='\0'; i=i+1) {
+        if(caracter[i] == ' ') { 
+            cout << endl;
+        } else {
+            cout << caracter[i]; 
+        }
+    }
+    return 0;
+}
+
