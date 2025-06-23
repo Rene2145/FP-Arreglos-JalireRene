9585a0962b7a51c0eb12bfca9d3ce6b48a07cf2c
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-06-23 04:55:52 -0500

    Ejercicio 6 – suma de componentes en distintos indices, versión final

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -2,15 +2,27 @@
 using namespace std;
 
 int main() {
-    int n, vector[]; 
-    int sPar = 0, 
-	int sImpar = 0;
-
-    cout << "¿Cuantos elementos deseas introducir?: ";
+    int n, vector[60]; 
+    int sP = 0, sI = 0;
+    cout << " SUMA DE COMPONENTES EN UN VECTOR"<<endl<<endl;
+    cout << "¿Cuantos numeros deseas introducir?"<<endl;
     cin >> n;
+    cout << "Escribe esos numeros en cada indice"<<endl;
+    for(int i = 0; i < n; i=i+1) {
+        cout << "Indice #" << i << ":"<<endl;
+        cin >> vector[i];
+    }
 
-    cout << sPar << endl;
-    cout << sImpar << endl;
+    for(int i = 0; i < n; i=i+1) {
+        if(i % 2 == 0) { 
+            sP = vector[i] + sP;
+        } else {        
+            sI = vector[i] + sI;
+        }
+    }
+    cout << endl<<"La suma de componentes en:"<< endl;
+    cout << "Indices pares   = " << sP << endl;
+    cout << "Indices impares = " << sI << endl;
 
     return 0;
 }
