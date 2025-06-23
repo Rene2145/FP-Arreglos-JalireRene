5e145eb5769048e7e533db084b0a8f361f86d475
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-06-23 11:05:40 -0500

    Ejercicio 11 – Ingreso de elementos del arreglo, versión final

--- ejercicio11.cpp
+++ ejercicio11.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
 	int n, sp=0, sn=0;
+	cout<<"SUMA DE ELEMENTOS POSITIVOS Y "<<endl<<"NEGATIVOS EN UN ARREGLO"<<endl<<endl;
 	cout<<"¿Cuantos numeros deseas introducir al arreglo?"<<endl;
 	cin>>n;
 	int a[n];
@@ -16,7 +17,7 @@ int main(){
 		}
 	}
 	cout<<"RESULTADOS"<<sp<<endl;
-	cout<<"Suma de positivos es: "<<sp<<endl;
-	cout<<"Suma de negativos es: "<<sn<<endl;
+	cout<<"La suma de numeros positivos es: "<<sp<<endl;
+	cout<<"La suma de numeros negativos es: "<<sn<<endl;
 	return 0;
 }
