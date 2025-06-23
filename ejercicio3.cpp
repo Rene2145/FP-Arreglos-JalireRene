8d436e572cb3cd4d8d371af64cb7b4dc312609fd
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-06-22 22:29:14 -0500

    Ejercicio 3 – buscador para la letra ingresada, versión final

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,5 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	return 0;
+
+int main() {
+    char caracter[60]; 
+    char l;      
+    int c = 0; 
+    cout << "OCURRENCIA DE UNA LETRA"<<endl<<endl;
+    cout << "Escribe una frase: "<<endl;
+    cin.getline(caracter, 100); 
+    cout << "Escribe la letra que deseas encontrar:"<<endl;
+    cin >> l; 
+    for(int i = 0; caracter[i] != '\0'; i=i+1) {
+        if(caracter[i] == l) { 
+            c=c+1; 
+        }
+    }
+    cout << "Pudimos encontrar " << c << " veces "<<"la letra "<<l<<"."<< endl;
+
+    return 0;
 }
+
