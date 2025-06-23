afe68d1e4477ffa77e5ea79eee62e217f7d7ee54
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-06-23 04:17:17 -0500

    Ejercicio 6 – Avance 1: Introduccion de datos

---
+++ ejercicio6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, vector[]; 
+    int sPar = 0, 
+	int sImpar = 0;
+
+    cout << "¿Cuantos elementos deseas introducir?: ";
+    cin >> n;
+
+    cout << sPar << endl;
+    cout << sImpar << endl;
+
+    return 0;
+}
+
